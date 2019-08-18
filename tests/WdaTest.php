bbd5a0758922e62110b0f24c70d5690ef8bd8b56
paooolino <paooolino@gmail.com>
2019-08-19 00:50:26 +0200

    tests

--- tests/WdaTest.php
+++ tests/WdaTest.php
@@ -4,24 +4,10 @@ use PHPUnit\Framework\TestCase;
 
 final class WdaTest extends TestCase {
   private $app;
-  
-  protected function setUp():void {
-    if (!file_exists('temp'))
-      mkdir('temp');
-    
-    if (!file_exists('temp/testapp')) {
-      mkdir('temp/testapp');
-      shell_exec("php generatecode.php tests/samples/sample.ini temp/testapp");
-      chdir('temp/testapp');
-      shell_exec('composer install');
-    }
-  }
-  
-  public function testCreate() {
-    // mock the index.php and run app to analyze responses
-  }
-  
-  public static function rrmdir($dir) { 
+  private $dir = __DIR__ . '/testapp';
+
+  // funzione di utilità per la rimozione di tutto il contenuto di una dir
+  private function rrmdir($dir) { 
     if (is_dir($dir)) { 
       $objects = scandir($dir); 
       foreach ($objects as $object) { 
@@ -34,6 +20,50 @@ final class WdaTest extends TestCase {
       }
       rmdir($dir); 
     } 
+  }
+  
+  // preparazione del test
+  private function prepareTest() {
+    mkdir($this->dir);
+    shell_exec("php generatecode.php tests/samples/sample.ini " . $this->dir);
+    chdir($this->dir);
+    shell_exec('composer install');
+  }
+  
+  // rimozione completa dei file creati durante il test
+  private function cleanupTest() {
+    $this->rrmdir($this->dir);
+  }
+  
+  protected function setUp():void {
+    // chiamare il prepareTest
+    if (file_exists($this->dir)) {
+      echo "Skipping installation. Please remove " . $this->dir . " directory if you want a complete test.";
+    } else {
+      $this->prepareTest();
+    }
+    require $this->dir . '/vendor/autoload.php';
+    $settings = require $this->dir . '/settings.php';
+    $app = new Slim\App($settings);
+    $container = $app->getContainer();
+    require $this->dir . '/app/dependencies.php';
+    require $this->dir . '/app/middleware.php';
+    require $this->dir . '/app/routes.php';
+    $this->app = $app;
+  }
+  
+  public function testHomeResponds() {
+    // mock the index.php and run app to analyze responses
+    $env = \Slim\Http\Environment::mock([
+      'REQUEST_METHOD' => 'GET',
+      'REQUEST_URI' => '/'
+    ]);
+    $request = \Slim\Http\Request::createFromEnvironment($env);
+    $response = new \Slim\Http\Response();
+    $response = $this->app->process($request, $response);
+    
+    $expected = 'Please edit the template source file in /templates/default/src/home';
+    $this->assertStringContainsString($expected, $response->getBody()->__toString());
   }
   
   public static function tearDownAfterClass():void {
